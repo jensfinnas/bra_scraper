65728731f2f2ce46d5ba78d552aeb5c3d85c6bb5
jensfinnas <jens.finnas@gmail.com>
2017-03-24 09:59:19 +0100

    Add explicit requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@
 
 from setuptools import setup
 
-# Get list of requirements
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 
 
 setup(name='bra_scraper',
-      version='0.1.2',
+      version='0.1.3',
       description=u'An interface for fetching statistical data from BRÅ. This is not an official service!',
       url='https://github.com/jensfinnas/bra_scraper',
       author='Jens Finnäs',
@@ -16,5 +13,8 @@ setup(name='bra_scraper',
       license='MIT',
       packages=['bra_scraper'],
       include_package_data=True,
-      install_requires=required,
+      install_requires=[
+            "requests==2.11.1",
+            "wsgiref==0.1.2"
+      ],
       zip_safe=False)
