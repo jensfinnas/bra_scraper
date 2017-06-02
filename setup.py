73e8acd87e96d281981ba97fee5c87d75f3d061b
Jens Finnäs <jens.finnas@gmail.com>
2017-06-02 13:48:45 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='bra_scraper',
-      version='0.1.4',
+      version='0.1.5',
       description=u'An interface for fetching statistical data from BRÅ. This is not an official service!',
       url='https://github.com/jensfinnas/bra_scraper',
       author='Jens Finnäs',
